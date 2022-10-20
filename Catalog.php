619be8631cdcd6419bdadb0ab9d36b7a2bf8903e
Aton, Shean Danes <sa372@students.waikato.ac.nz>
2022-10-21 11:50:21 +1300

    Our second commit

--- Catalog.php
+++ Catalog.php
@@ -4,7 +4,12 @@
 
 <?php
 $xml=simplexml_load_file("catalog.xml") or die("Error Cannot create object");
-print_r($xml);
+
+    foreach($xml->children() as $xml){
+        echo"<div class='movieContainer'>
+            <img class='images' src=$xml->image>
+        </div>";
+    }
 ?>
 
 </body>
