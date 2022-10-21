c7b36355cee4993fb5990c83695447e409b3d324
Aton, Shean Danes <sa372@students.waikato.ac.nz>
2022-10-21 15:58:09 +1300

    Added some styling

--- php/Catalog.php
+++ php/Catalog.php
@@ -2,14 +2,30 @@
 <html>
 <body>
 
-<?php
-$xml=simplexml_load_file("../xml/catalog.xml") or die("Error Cannot create object");
+<head>
+        <link rel="stylesheet" href="../css/stylesheet.css" type="text/css">
+</head>
 
+<?php
+    $xml=simplexml_load_file("../xml/catalog.xml") or die("Error Cannot create object");
+    echo"<div class='header'>
+    <h1>MARVEL INFINITY SAGA</h1>
+    </div>";
+
+
+    echo"<div id='marvelMovies'>";
     foreach($xml->children() as $xml){
-        echo"<div class='movieContainer'>
-            <img class='images' src=$xml->image>
-        </div>";
+          echo"
+          <center>
+          <div class='movieContainer'>
+              <div class='movieTitle'>
+              <img class='images' src=$xml->image>
+                  <h3>$xml->title</h3>
+              </div>
+          </div>
+          </center>";
     }
+  echo"</div>";
 ?>
 
 </body>
