616805819462a2ac8e8dc26cfd209417082ee33b
Aton, Shean Danes <sa372@students.waikato.ac.nz>
2022-10-25 12:09:59 +1300

    Here are new updates

--- php/Catalog.php
+++ php/Catalog.php
@@ -14,16 +14,50 @@
 
 
     echo"<div id='marvelMovies'>";
-    foreach($xml->children() as $xml){
-          echo"
-          <center>
-            <div class='movieContainer'>
-                        <img class='images' src=$xml->image>
-                        <h3>$xml->title</h3>
-            </div>
-          </center>";
-    }
-    echo"<div id='sort-container'></div>";
+      foreach($xml->children() as $xml){
+            echo"
+            <center>
+              <div class='movieContainer'>
+                          <img class='images' src=$xml->image>
+                          <h3>$xml->title</h3>
+              </div>
+            </center>";
+      }
+    echo"<div id='sort-container'>
+              <form class='movie-form'>
+                <div class='grid-box'>
+                  <div class='grid-child'>
+                    <label for='SEARCH' class='search-label'>SEARCH</label><br>
+                    <input type='text' id='inputID-search' class='search-block' name='SEARCH' placeholder='Movie Name'><br>
+                  </div> 
+
+                  <div class='grid-child'>
+                    <label for='ORDER'  class='search-label'>ORDER</label><br>
+                    <span class='error-msg' id='error-order'>Please select an option</span><br> 
+                    <select id='ORDER' name='ORDER' class='search-block>
+                        <option name='order1' value='releaseDate'>Release Date</option>
+                        <option name='order2' value='alphabetically'>Alphabetically</option>
+                        <option name='order3' value='rating'>Rating</option>
+                    </select> 
+                  </div>
+                  
+                  <div class='grid-child'>
+                    <label for='ORDERBY'  class='search-label'>ORDER BY</label><br>
+                    <span class='error-msg' id='error-orderBy'>Please select an option</span><br> 
+                    <select id='ORDERBY' name='ORDERBY' class='search-block'>
+                        <option name='orderBy1' value='asc'>Ascending</option>
+                        <option name='orderBy2' value='desc'>Descending</option>
+                        <option name='orderBy3' value='random'>Random</option>
+                    </select> 
+                  </div> 
+
+                  <div class'grid-child'>
+                    <input type='submit' name='submit' value='submit' class='search-block' >
+                  </div> 
+                
+                </div>               
+              </form> 
+          </div>";
   echo"</div>";
 
 ?>
