cdcb0c3c5c1ebff787b07769fb96aa9fbeb2a3bc
Aton, Shean Danes <sa372@students.waikato.ac.nz>
2022-10-28 11:11:42 +1300

    search thingy works

--- php/Catalog.php
+++ php/Catalog.php
@@ -7,6 +7,7 @@
 
 <?php
     $x=0;
+    $searchMovie="";
     $xml=simplexml_load_file("../xml/catalog.xml") or die("Error Cannot create object");
     echo"<div class='header'>
     <h1>MARVEL INFINITY SAGA</h1>
@@ -14,28 +15,35 @@
 
     echo"<div id='marvelMovies'>";
         foreach($xml->children() as $xml){
+            $x++;
             $mName=$xml->title;
             $img =$xml->image;
             $imdbR=$xml->rating;
             $director=$xml->director;
             $MVlength =$xml->length; 
             echo"
-                <center>
+            <div class='movieholder'id=$x>
+                <center class='center'>
                     <a href='Details.php?t=$mName&rt=$imdbR&img=$img&dt=$director&l=$MVlength'>
                         <div class='movieContainer'>
                                 <img class='images' src=$img>
-                                    <h3>$mName</h3>            
+                                    <h3 id=$x>$mName</h3>            
                         </div>
                     </a>
-                </center>";
+                </center>
+            </div>"    
+            ;
         }
-        echo"<div id='sort-container'>
-                <form class='movie-form'>
+  echo"</div>";
+?>
+    <div id='sort-container'>
+                <form class='movie-form' onsubmit="return false">
                     <div class='grid-box'>
-                        <div class='grid-child'>
-                        <label for='SEARCH' class='search-label'>SEARCH</label><hr>
-                        <input type='text' id='inputID-search' class='search-block' name='SEARCH' placeholder='Movie Name'>
-                        </div> 
+                        <div class="grid-child">
+                            <label for="SEARCH" class='search-label'>SEARCH</label><hr>
+                            <input type="text" id="inputID-search" class="search-block" name="SEARCH" placeholder="Movie Name">
+                            <input type='submit' id='search-button' name='submit' value='SEARCH' class='search-block'>
+                        </div>
 
                         <div class='grid-child'>
                         <label for='ORDER'  class='search-label'>ORDER</label><hr>
@@ -44,28 +52,72 @@
                             <option name='order2' value='alphabetically'>Alphabetically</option>
                             <option name='order3' value='rating'>Rating</option>
                         </select> 
-                        <span class='error-msg' id='error-order'>Please select an option</span>
                         </div>
                         
                         <div class='grid-child'>
-                        <label for='ORDERBY'  class='search-label'>ORDER BY</label><hr>
+                        <label for='ORDERBY'  class='search-label'>ORDER BY</label><hr>   
                         <select id='ORDERBY' name='ORDERBY' class='search-block'>
                             <option name='orderBy1' value='asc'>Ascending</option>
                             <option name='orderBy2' value='desc'>Descending</option>
                             <option name='orderBy3' value='random'>Random</option>
                         </select> 
-                        <span class='error-msg' id='error-orderBy'>Please select an option</span>
                         </div> 
 
-                        <div class'grid-child'>
-                            <input type='submit' id='sort-button' name='submit' value='SORT' class='search-block'>
+                        <div class='grid-child'>
+                            <input type='submit' onlick="SubmitButton()" id='sort-button' name='submit' value='SORT' class='search-block'>
                         </div> 
                     </div>               
                 </form> 
-            </div>";
-  echo"</div>";
+            </div>
+        
+    <script type="text/javascript">
+        var input =  document.getElementById("inputID-search").value;
+        console.log(input);
+        
+        var moviesContainer = document.querySelectorAll("center");
+        console.log(moviesContainer);
+        var movies = document.querySelectorAll("div.movieContainer > h3");
+        console.log(document.querySelectorAll(".movieContainer > h3")[1].innerText);
 
-?>
 
+        const marvelDiv = document.getElementById("marvelMovies").innerHTML;
+        console.log(marvelDiv);
+        
+        document.getElementById("search-button").addEventListener("click", function() {
+            var searchedMovie = document.getElementById("inputID-search").value;
+            console.log(searchedMovie);
+            if(searchedMovie.length ==0){
+                alert("No movie was searched. Please enter it the movie search bar. Thank you!");
+            }
+            for(var i =0; i<24; i++){
+                var movieTitles=document.querySelectorAll(".movieContainer > h3")[i].innerHTML;
+                console.log(movieTitles);
+                if( searchedMovie.toLowerCase()==movieTitles.toLowerCase()){
+                    
+                    var movieDisplay = document.querySelectorAll(".movieholder")[i].innerHTML; 
+                    console.log(movieDisplay + "movie :" + movieTitles.toLowerCase());
+                    document.getElementById("marvelMovies").innerHTML = '';
+                    document.getElementById("marvelMovies").innerHTML=movieDisplay;
+                    
+                }
+            }
+        });
+    
+        input.addEventListener('input', updateValue);
+
+        function updateValue(e) {
+        log.textContent = e.target.value;
+        }
+
+        function SubmitButton(event){
+            var textInput = document.getElementById("inputID-search").value;
+            var orderInput = document.getElementById("ORDER").value;
+            var orderByInput = document.getElementById("ORDERBY").value;
+
+            document.getElementById("header").innerHTML ="hello";
+        }
+
+
+    </script>        
 </body>
 </html>
